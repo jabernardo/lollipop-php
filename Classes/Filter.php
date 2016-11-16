6430ff5c50b71b185be8cbe6db21a70e46ae41bd
John Aldrich Bernardo <bjohnaldrich@gmail.com>
2016-11-16 07:01:54 +0000

    filter.php: adding more filters

--- Classes/Filter.php
+++ Classes/Filter.php
@@ -4,7 +4,7 @@
     /**
      * Filter Class
      *
-     * @version     1.0
+     * @version     1.2
      * @author      John Aldrich Bernardo
      * @email       4ldrich@protonmail.com
      * @package     Lollipop 
@@ -17,33 +17,55 @@
          * Checks string if is a valid name
          *
          * @param   string  $string	String
-         *
-         * @return  bool
+         * @return  mixed   Returns <false> if string is not valid for name usage
+         * 
          */
         static function name($string) {
-            return (!preg_match('/[0-9!@#$%\^&\*\(\)\+_\\\|=\[\]\{\}\<\>\/?\,;:\'\"~`]/', $string) && strlen($string) > 1);
+            return (!preg_match('/[0-9!@#$%\^&\*\(\)\+_\\\|=\[\]\{\}\<\>\/?\,;:\'\"~`]/', $string) && strlen($string) > 1) ? $string : false;
         }
 
         /**
          * Checks string if is a valid contact number_format
          *
          * @param   string  $string     String
-         *
-         * @return  bool
+         * @return  mixed   Returns <false> if string is not valid for mobile or telephone number
+         * 
          */
         static function contact($string) {
-            return (preg_match('/\+[0-9]{12}|[0-9]{11}|[0-9]{3}-[0-9]{3}-[0-9]{4}/', $string));
+            return (preg_match('/\+[0-9]{12}|[0-9]{11}|[0-9]{3}-[0-9]{3}-[0-9]{4}/', $string)) ? $string : false;
         }
 
         /**
          * Checks string is is a valid email address
          *
          * @param   string  $string     String
-         *
-         * @return  bool
+         * @return  bool    Returns <false> if string is not valid for email usage
+         * 
          */
         static function email($string) {
-            return filter_var($string, FILTER_VALIDATE_EMAIL) ? true : false;
+            return filter_var($string, FILTER_VALIDATE_EMAIL) ? $string : false;
+        }
+        
+        /**
+         * Check if string is a valid URL
+         * 
+         * @param   string  $string     String
+         * @return  bool    Returns <false> if string is not valid for url
+         * 
+         */
+        static function url($string) {
+            return filter_var($string, FILTER_VALIDATE_URL) ? $string : false;
+        }
+        
+        /**
+         * Check if string is a valid IP
+         * 
+         * @param   string  $string     String
+         * @return  bool    Returns <false> if string is not valid for ip
+         * 
+         */
+        static function ip($string) {
+            return filter_var($string, FILTER_VALIDATE_IP) ? $string : false;
         }
     }
 ?>
