8c254f1366f56bdd18d60988514014cb9c0b386c
John Aldrich Bernardo <4ldrich@protonmail.com>
2017-03-27 16:42:05 +0800

    autoload: less tab

--- autoload.php
+++ autoload.php
@@ -1,97 +1,98 @@
 <?php
-    /**
-     * Lollipop Autoload 
-     * 
-     * @version 4.0.0
-     * @author  John Aldrich Bernardo
-     * 
-     */
-     
-    /**
-     * Document root
-     * 
-     */
-    define('DOCUMENT_ROOT', str_replace(DIRECTORY_SEPARATOR, '/', $_SERVER['DOCUMENT_ROOT']));
+
+/**
+ * Lollipop Autoload 
+ * 
+ * @version 4.0.0
+ * @author  John Aldrich Bernardo
+ * 
+ */
+ 
+/**
+ * Document root
+ * 
+ */
+define('DOCUMENT_ROOT', str_replace(DIRECTORY_SEPARATOR, '/', $_SERVER['DOCUMENT_ROOT']));
+
+/**
+ * Lollipop directory
+ * 
+ * 
+ */
+define('LOLLIPOP_BASE', str_replace(DIRECTORY_SEPARATOR, '/', dirname(__FILE__) . '/'));
+
+/**
+ * Lollipop classes directory
+ * 
+ * 
+ */
+define('LOLLIPOP_LIBRARY', str_replace(DIRECTORY_SEPARATOR, '/',  LOLLIPOP_BASE . 'Library/'));
+
+/**
+ * Lollipop Storage directory
+ * 
+ * 
+ */
+define('LOLLIPOP_STORAGE', str_replace(DIRECTORY_SEPARATOR, '/',  LOLLIPOP_BASE . 'Storage/'));
+
+/**
+ * Lollipop cache directory
+ * 
+ * 
+ */
+define('LOLLIPOP_STORAGE_CACHE', str_replace(DIRECTORY_SEPARATOR, '/',  LOLLIPOP_STORAGE . 'cache/'));
+
+/**
+ * Lollipop cache directory
+ * 
+ * 
+ */
+define('LOLLIPOP_STORAGE_LOG', str_replace(DIRECTORY_SEPARATOR, '/',  LOLLIPOP_STORAGE . 'logs/'));
+
+/**
+ * Check if PHP version is valid
+ * 
+ */
+if (!function_exists('phpversion')) {
+    exit('You PHP is too old! Try upgrading.');
+}
+
+$_lol_toks = explode('.', phpversion());
+
+if (count($_lol_toks) >= 2) {
+    $_lol_major_minor = (double)($_lol_toks[0] . '.' . $_lol_toks[1]);
     
     /**
-     * Lollipop directory
-     * 
-     * 
-     */
-    define('LOLLIPOP_BASE', str_replace(DIRECTORY_SEPARATOR, '/', dirname(__FILE__) . '/'));
-    
-    /**
-     * Lollipop classes directory
-     * 
+     * if PHP version is 5.3 or below exit
      * 
      */
-    define('LOLLIPOP_LIBRARY', str_replace(DIRECTORY_SEPARATOR, '/',  LOLLIPOP_BASE . 'Library/'));
-    
-    /**
-     * Lollipop Storage directory
-     * 
-     * 
-     */
-    define('LOLLIPOP_STORAGE', str_replace(DIRECTORY_SEPARATOR, '/',  LOLLIPOP_BASE . 'Storage/'));
-    
-    /**
-     * Lollipop cache directory
-     * 
-     * 
-     */
-    define('LOLLIPOP_STORAGE_CACHE', str_replace(DIRECTORY_SEPARATOR, '/',  LOLLIPOP_STORAGE . 'cache/'));
-    
-    /**
-     * Lollipop cache directory
-     * 
-     * 
-     */
-    define('LOLLIPOP_STORAGE_LOG', str_replace(DIRECTORY_SEPARATOR, '/',  LOLLIPOP_STORAGE . 'logs/'));
-    
-    /**
-     * Check if PHP version is valid
-     * 
-     */
-    if (!function_exists('phpversion')) {
+    if ($_lol_major_minor < (5.4)) {
         exit('You PHP is too old! Try upgrading.');
     }
-    
-    $_lol_toks = explode('.', phpversion());
-    
-    if (count($_lol_toks) >= 2) {
-        $_lol_major_minor = (double)($_lol_toks[0] . '.' . $_lol_toks[1]);
-        
-        /**
-         * if PHP version is 5.3 or below exit
-         * 
-         */
-        if ($_lol_major_minor < (5.4)) {
-            exit('You PHP is too old! Try upgrading.');
-        }
-    } else {
-        exit('The version of your PHP can\'t be verified');
-    }
-    
-    /**
-     * __autoload function
-     *
-     */
-    function autoLoader($class) {
-        $tokens = explode('\\', $class);
+} else {
+    exit('The version of your PHP can\'t be verified');
+}
 
-        if (count($tokens) == 2) {
-            $file = LOLLIPOP_LIBRARY . ucfirst($tokens[1]) . '.php';
+/**
+ * __autoload function
+ *
+ */
+function autoLoader($class) {
+    $tokens = explode('\\', $class);
 
-            if (file_exists($file)) {
-                require_once($file);
-            }
+    if (count($tokens) == 2) {
+        $file = LOLLIPOP_LIBRARY . ucfirst($tokens[1]) . '.php';
+
+        if (file_exists($file)) {
+            require_once($file);
         }
     }
+}
 
-    // Register autoloader
-    spl_autoload_register('autoLoader');
-    
-    // Include namespace
-    require_once(LOLLIPOP_BASE . 'lollipop.php');
-    
+// Register autoloader
+spl_autoload_register('autoLoader');
+
+// Include namespace
+require_once(LOLLIPOP_BASE . 'lollipop.php');
+
 ?>
