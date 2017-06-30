b623d344b415b82a4017a4edcac3de5e194fee33
John Aldrich Bernardo <bjohnaldrich@gmail.com>
2017-06-30 10:25:07 +0000

    TestSuite: Text

---
+++ Test/TextTest.php
@@ -0,0 +1,25 @@
+<?php
+
+if (file_exists('PHPUnit/Autoload.php'))
+    require_once('PHPUnit/Autoload.php');
+
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
+use \Lollipop\Text;
+use \PHPUnit\Framework\TestCase;
+
+class TextTest extends TestCase
+{
+    public function testLock() {
+        $locked = Text::lock('hello', 12345);
+        
+        $this->assertEquals(
+                'hello',
+                Text::unlock($locked, 12345)
+            );
+    }
+}
