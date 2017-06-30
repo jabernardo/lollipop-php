05fc8914928456522772aaa51c7bdc58debf0011
John Aldrich Bernardo <bjohnaldrich@gmail.com>
2017-06-30 09:37:20 +0000

    TestSuite: Database

---
+++ Test/DatabaseTest.php
@@ -0,0 +1,27 @@
+<?php
+
+if (file_exists('PHPUnit/Autoload.php'))
+    require_once('PHPUnit/Autoload.php');
+
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
+use \Lollipop\Database;
+use \PHPUnit\Framework\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    public function testObject() {
+        $db = Database::table('messages')
+            ->where('id', 1)
+            ->selectAll();
+            
+        $this->assertEquals(
+                'SELECT * FROM messages WHERE id = \'1\'',
+                (string)$db
+            );
+    }
+}
