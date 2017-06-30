2c98b56da7d0c9989228a55cf29138d88337d00d
John Aldrich Bernardo <bjohnaldrich@gmail.com>
2017-06-30 09:37:03 +0000

    TestSuite: Config

---
+++ Test/ConfigTest.php
@@ -0,0 +1,35 @@
+<?php
+
+if (file_exists('PHPUnit/Autoload.php'))
+    require_once('PHPUnit/Autoload.php');
+
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
+use \Lollipop\Config;
+use \PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    public function testAdd() {
+        Config::add('cache', true);
+        
+        $this->assertEquals(
+                true,
+                Config::get('cache')
+            );
+    }
+    
+    public function testRemove() {
+        Config::add('cache', true);
+        Config::remove('cache');
+        
+        $this->assertEquals(
+                null,
+                Config::get('cache')
+            );
+    }
+}
