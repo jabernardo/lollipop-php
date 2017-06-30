625b89b983fd60266045353fdc494e4c43a03fc8
John Aldrich Bernardo <bjohnaldrich@gmail.com>
2017-06-30 03:37:08 +0000

    TestSuite: backward compatibility

--- Test/CacheTest.php
+++ Test/CacheTest.php
@@ -3,6 +3,12 @@
 if (file_exists('PHPUnit/Autoload.php'))
     require_once('PHPUnit/Autoload.php');
 
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
 use \Lollipop\Cache;
 use \PHPUnit\Framework\TestCase;
 
