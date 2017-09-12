8ddbf840deee4f9abf8dac2b05c3e1970d8cecde
John Aldrich Bernardo <bjohnaldrich@gmail.com>
2017-09-12 02:38:45 +0000

    CsrfTokenTest: initial commit

---
+++ Test/CsrfTokenTest.php
@@ -0,0 +1,23 @@
+<?php
+
+if (file_exists('PHPUnit/Autoload.php'))
+    require_once('PHPUnit/Autoload.php');
+
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
+use \Lollipop\CsrfToken;
+use \PHPUnit\Framework\TestCase;
+
+class CsrfTokenTest extends TestCase
+{
+    public function testToken() {
+        $this->assertEquals(
+                true,
+                CsrfToken::isValid(CsrfToken::get())
+            );
+    }
+}
