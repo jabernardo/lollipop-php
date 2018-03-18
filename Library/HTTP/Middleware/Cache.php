4f8d828f99b3f03dd9f027a8f2565e13c4fdb269
John Aldrich Bernardo <4ldrich@protonmail.com>
2018-03-18 22:02:03 +0800

    Middleware: remove sapi checking

--- Library/HTTP/Middleware/Cache.php
+++ Library/HTTP/Middleware/Cache.php
@@ -4,15 +4,6 @@ namespace Lollipop\HTTP\Middleware;
 
 defined('LOLLIPOP_BASE') or die('Lollipop wasn\'t loaded correctly.');
 
-/**
- * Check application if running on web server
- * else just terminate
- * 
- */
-if (!isset($_SERVER['REQUEST_URI'])) {
-    exit('Lollipop Application must be run on a web server.' . PHP_EOL);
-}
-
 use \Lollipop\Config;
 use \Lollipop\HTTP\Response;
 use \Lollipop\HTTP\Route;
