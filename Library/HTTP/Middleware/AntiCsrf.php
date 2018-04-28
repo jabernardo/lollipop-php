e63edacfe6ef12c480439f9bbd0f9a9e6a83ae01
John Aldrich Bernardo <jabernardo@usautoparts.com>
2018-04-28 17:07:11 +0800

    Middleware: AntiCsrf: refactor checking

--- Library/HTTP/Middleware/AntiCsrf.php
+++ Library/HTTP/Middleware/AntiCsrf.php
@@ -14,7 +14,7 @@ use \Lollipop\HTTP\Middleware;
 /**
  * Lollipop AntiCsrf Middleware
  *
- * @version     1.1.3
+ * @version     1.1.4
  * @author      John Aldrich Bernardo
  * @email       4ldrich@protonmail.com
  * @package     Lollipop
@@ -35,33 +35,33 @@ class AntiCsrf implements Middleware
         $acsrf_enable = Config::get('anti_csrf.enable', true);
         $acsrf_name = CsrfToken::getName();
         $expiration = Config::get('anti_csrf.expiration', 18000);
+        $failed = false;
         
-        if (!$req->isMethod('get') && $acsrf_enable) {
-            if (!CsrfToken::isValid($req->header($acsrf_name)) && !CsrfToken::isValid($req->get($acsrf_name))) {
-                $output = '<!DOCTYPE html>'
-                        . '<!-- Lollipop for PHP by John Aldrich Bernardo -->'
-                        . '<html>'
-                        . '<head>'
-                        . '<title>Not Enough Tokens</title>'
-                        . '<meta name="viewport" content="width=device-width, initial-scale=1">'
-                        . '</head>'
-                        . '<body>'
-                        . '<h1>Not Enough Tokens</h1>'
-                        . '<p>Oops! Make sure you have enough tokens before you can play.</p>'
-                        . '</body>'
-                        . '</html>';
-                
-                $output_config = Config::get('output');
-                $output_compression = !is_null($output_config) && isset($output_config->compression) && $output_config->compression;
-                
-                $res = new Response($output);
-                $res->compress($output_compression);
-                $res->render();
-                exit();
-            }
+        if (!$req->isMethod('get') && $acsrf_enable &&
+            !CsrfToken::isValid($req->header($acsrf_name)) && !CsrfToken::isValid($req->get($acsrf_name))) {
+            $output = '<!DOCTYPE html>'
+            . '<!-- Lollipop for PHP by John Aldrich Bernardo -->'
+            . '<html>'
+            . '<head>'
+            . '<title>Not Enough Tokens</title>'
+            . '<meta name="viewport" content="width=device-width, initial-scale=1">'
+            . '</head>'
+            . '<body>'
+            . '<h1>Not Enough Tokens</h1>'
+            . '<p>Oops! Make sure you have enough tokens before you can play.</p>'
+            . '</body>'
+            . '</html>';
+        
+            $output_config = Config::get('output');
+            $output_compression = !is_null($output_config) && isset($output_config->compression) && $output_config->compression;
+            
+            $res = new Response($output);
+            $res->compress($output_compression);
+
+            $failed = true;
         }
-        
-        $res = $next($req, $res);
+
+        if (!$failed) $res = $next($req, $res);
 
         return $res;
     }
